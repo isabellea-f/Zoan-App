2d5d752970cfcbec4d24423c6ffbf90279e6e08b
Max Mattiasson <max@mattiasson.eu>
2026-03-03 13:17:31 +0100

    test

--- app/timer.jsx
+++ app/timer.jsx
@@ -1,8 +1,25 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
 export default function Timer() {
+  const [number, onChangeNumber] = React.useState("45");
+
   return (
     <View className="flex-1 items-center justify-center">
+      <TextInput
+        value={number}
+        onChangeText={onChangeNumber}
+        inputMode="numeric"
+        maxLength={3}
+      />
+      <Button
+        title="+"
+        onPress={() => onChangeNumber((prev) => (Number(prev) + 5).toString())}
+      />
+      <Button
+        title="-"
+        onPress={() => onChangeNumber((prev) => (Number(prev) - 5).toString())}
+      />
       <Text>Second Screen</Text>
     </View>
   );
