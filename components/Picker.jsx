df15bc67c401e0a601336ef2e240fffd5d736854
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2026-03-09 15:01:09 +0100

    style(todo): update layout and colors in todo app

--- components/Picker.jsx
+++ components/Picker.jsx
@@ -1,8 +1,9 @@
 import { Picker } from "@react-native-picker/picker";
+import { View } from "react-native";
 
 export function DropdownPicker({ value, onValueChange, options }) {
   return (
-    <>
+    <View style={{ width: 120 }}>
       <Picker
         selectedValue={value}
         onValueChange={(itemValue) => onValueChange(itemValue)}
@@ -11,6 +12,6 @@ export function DropdownPicker({ value, onValueChange, options }) {
           <Picker.Item key={option} label={option} value={option} />
         ))}
       </Picker>
-    </>
+    </View>
   );
 }
